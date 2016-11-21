fd79afe9f40b9f8cc7fdcd97786936fb80b094fb
LU YI <tsb1050391@gmail.com>
2016-11-21 15:31:45 +0800

    Load API key function

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,26 @@
-buildQueryBody = function(...) {
+buildQueryBody <- function(...) {
   docs = list(...)
-  docs$sep = ","
-  sprintf('{"documents": [%s]}',do.call(paste, docs))
+  sprintf('{"documents": [%s]}', paste(docs, sep = ",", collapse = ","))
+}
+
+#' Import API keys
+#'
+#' @param path Path to API keys (yaml format). See Details.
+#'
+#' @details
+#' Default path to API keys file is ~/cognitive_api_keys.txt.
+#'
+#' \preformatted{
+#' Text-Analytics: xxxxxx
+#' Face: xxxxxxx
+#' }
+#'
+#'
+#' @import yaml
+#' @export
+load_api_key <- function(path = normalizePath("~/cognitive_api_keys.txt"),
+                         product = c("Text-Analytics", "Face")) {
+  product <- match.arg(product)
+  keys <- yaml::yaml.load_file(path)
+  keys[[pruduct]]
 }
